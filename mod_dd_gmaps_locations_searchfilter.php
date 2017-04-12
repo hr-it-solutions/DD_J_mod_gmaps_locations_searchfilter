af95f5535b484a3eb4c0349ded7aaeeb0bf76158
Florian Häusler <florian@didldu.com>
2017-04-12 14:51:47 +0200

    Added entrie point Implemented scripts and stylesheet jimport component helper to load component params Linked google js Implemented helper

--- mod_dd_gmaps_locations_searchfilter.php
+++ mod_dd_gmaps_locations_searchfilter.php
@@ -8,7 +8,23 @@
 
 defined('_JEXEC') or die;
 
+jimport('joomla.application.component.helper');
+
 // Include the functions only once
 JLoader::register('ModDD_GMaps_Locations_SearchFilter_Helper', __DIR__ . '/helper.php');
+
+$componentParams = JComponentHelper::getParams('com_dd_gmaps_locations');
+
+$doc = JFactory::getDocument();
+
+$google_PlacesAPI = 'js?&libraries=places&v=3';
+$google_PlacesAPI_Key = '&key=' . $componentParams->get('google_api_key_js_places');
+
+
+$doc->addScript('https://maps.google.com/maps/api/' . $google_PlacesAPI . '&key=' . $google_PlacesAPI_Key);
+
+
+$doc->addStyleSheet(JUri::base() . 'media/mod_dd_gmaps_locations_searchfilter/css/dd_gmaps_locations_searchfilter.css');
+
 
 require JModuleHelper::getLayoutPath('mod_dd_gmaps_locations_searchfilter', $params->get('layout', 'default'));
