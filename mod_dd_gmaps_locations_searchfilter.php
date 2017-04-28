d1570a49707f8449b90d15fb7b763bfb98c76d0f
Florian Häusler <florian@didldu.com>
2017-04-29 00:45:12 +0200

    Improved code style phpcs Joomla New legal notes / copyright header

--- mod_dd_gmaps_locations_searchfilter.php
+++ mod_dd_gmaps_locations_searchfilter.php
@@ -1,7 +1,8 @@
 <?php
 /**
- * @version    1-1-0-0 // Y-m-d 2017-04-06
- * @author     HR IT-Solutions Florian Häusler https://www.hr-it-solutions.com
+ * @package    DD_GMaps_Locations_Searchfilter
+ *
+ * @author     HR IT-Solutions Florian Häusler <info@hr-it-solutions.com>
  * @copyright  Copyright (C) 2011 - 2017 Didldu e.K. | HR IT-Solutions
  * @license    http://www.gnu.org/licenses/gpl-2.0.html GNU/GPLv2 only
  **/
@@ -20,7 +21,7 @@ $doc = JFactory::getDocument();
 $google_PlacesAPI = 'js?&libraries=places&v=3';
 $google_PlacesAPI_Key = '&key=' . $componentParams->get('google_api_key_js_places');
 
-if(!ModDD_GMaps_Locations_SearchFilter_Helper::isset_Script($doc->_scripts, $google_PlacesAPI))
+if (!ModDD_GMaps_Locations_SearchFilter_Helper::isset_Script($doc->_scripts, $google_PlacesAPI))
 {
 	$doc->addScript('https://maps.google.com/maps/api/' . $google_PlacesAPI . '&key=' . $google_PlacesAPI_Key);
 }
