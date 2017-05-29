92352910d3844596e573c5a5e60ccdcd47766a29
Florian Häusler <florian@didldu.com>
2017-05-29 17:58:26 +0200

    Revision

--- mod_dd_gmaps_locations_searchfilter.php
+++ mod_dd_gmaps_locations_searchfilter.php
@@ -18,12 +18,12 @@ $componentParams = JComponentHelper::getParams('com_dd_gmaps_locations');
 
 $doc = JFactory::getDocument();
 
-$google_PlacesAPI = 'js?&libraries=places&v=3';
-$google_PlacesAPI_Key = '&key=' . $componentParams->get('google_api_key_js_places');
+$Places_API = 'js?&libraries=places&v=3';
+$API_Key = '&key=' . $componentParams->get('google_api_key_js_places');
 
-if (!ModDD_GMaps_Locations_SearchFilter_Helper::isset_Script($doc->_scripts, $google_PlacesAPI))
+if (!ModDD_GMaps_Locations_SearchFilter_Helper::isset_Script($doc->_scripts, $Places_API))
 {
-	$doc->addScript('https://maps.google.com/maps/api/' . $google_PlacesAPI . '&key=' . $google_PlacesAPI_Key);
+	$doc->addScript('https://maps.google.com/maps/api/' . $Places_API . '&key=' . $API_Key);
 }
 
 $doc->addStyleSheet(JUri::base() . 'media/mod_dd_gmaps_locations_searchfilter/css/dd_gmaps_locations_searchfilter.min.css');
