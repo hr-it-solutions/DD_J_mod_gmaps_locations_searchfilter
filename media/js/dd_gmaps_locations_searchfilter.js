19b7cafef621c8ccce14739eb6b3b259d3a47fe2
Florian Häusler <florian@didldu.com>
2017-10-27 00:28:27 +0200

    optimization

--- media/js/dd_gmaps_locations_searchfilter.js
+++ media/js/dd_gmaps_locations_searchfilter.js
@@ -7,20 +7,21 @@
  **/
 
 ;function submitDD_GMaps_Form() {
-    setTimeout(function(){
-        if(document.getElementById("dd_input_location_search").value != ""){
+    setTimeout(function() {
+        var address = jQuery('#dd_input_location_search');
+        if(address.val !== '') {
             geocoder = new google.maps.Geocoder();
-            var address = document.getElementById("dd_input_location_search").value;
-            geocoder.geocode( { 'address': address}, function(results, status) {
-                if (status == google.maps.GeocoderStatus.OK) {
-                    document.getElementById("dd_input_geolocate").value = "";
-                    document.getElementById("locationLatLng").value = results[0].geometry.location.lat() + ',' + results[0].geometry.location.lng();
+            geocoder.geocode( { 'address': address.val}, function(results, status) {
+                if (status === google.maps.GeocoderStatus.OK) {
+                    jQuery('#dd_input_geolocate').val = '';
+                    jQuery('#locationLatLng').val = results[0].geometry.location.lat() + ',' + results[0].geometry.location.lng();
                 }
             });
         }
     }, 300);
 
-    jQuery('#dd_searchfilter-ajaxloader').css({"display":"inline"});
+    jQuery('#dd_searchfilter-ajaxloader').css({'display':'inline'});
+
     setTimeout(function(){
         jQuery('#dd_gmaps_locations_searchfilter_form ').submit();
     }, 800);
@@ -28,17 +29,18 @@
 
 var initAutoCompleteListener = function initAutoCompleteListener() {
     // Adds auto complete input and input to LatLng function after google suggest list selected
-    var input = document.getElementById("dd_input_location_search");
+    var input = jQuery('#dd_input_location_search');
     var options = {types:[]};
     var autocomplete = new google.maps.places.Autocomplete(input, options);
 
-    google.maps.event.addListener(autocomplete, "place_changed", function() {
+    google.maps.event.addListener(autocomplete, 'place_changed', function() {
         var pl = autocomplete.getPlace();
-        jQuery("#locationLatLng").val(pl.geometry.location.lat() + "," + pl.geometry.location.lng()) ;
+        jQuery("#locationLatLng").val(pl.geometry.location.lat() + ',' + pl.geometry.location.lng()) ;
 
-        document.getElementById("dd_input_geolocate").value = "";
+        jQuery('#dd_input_geolocate').value = '';
 
         jQuery('#dd_searchfilter-ajaxloader').css({"display":"inline"});
+
         setTimeout(function(){
             jQuery('#dd_gmaps_locations_searchfilter_form ').submit();
         }, 400);
@@ -56,7 +58,7 @@ function clearDD_GMaps_Form() {
 }
 
 function showPosition(position) {
-    document.getElementById("dd_input_geolocate").value="locate";
-    document.getElementById("locationLatLng").value=position.coords.latitude+','+position.coords.longitude;
-    document.getElementById("dd_gmaps_locations_searchfilter_form").submit();
+    jQuery('#dd_input_geolocate').val = 'locate';
+    jQuery('#locationLatLng').val = position.coords.latitude + ',' + position.coords.longitude;
+    jQuery('#dd_gmaps_locations_searchfilter_form').submit();
 }
