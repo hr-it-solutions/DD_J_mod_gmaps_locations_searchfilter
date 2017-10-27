c8363b51b716eeabd8ddabfc89db4916e6b71c0a
Florian Häusler <florian@didldu.com>
2017-10-27 13:06:44 +0200

    jQuery bugfix ++ script improved

--- media/js/dd_gmaps_locations_searchfilter.js
+++ media/js/dd_gmaps_locations_searchfilter.js
@@ -7,21 +7,24 @@
  **/
 
 ;function submitDD_GMaps_Form() {
-    setTimeout(function() {
+    setTimeout(function(){
+
         var address = jQuery('#dd_input_location_search');
-        if(address.val !== '') {
+
+        if(address.val() !== ''){
             geocoder = new google.maps.Geocoder();
-            geocoder.geocode( { 'address': address.val}, function(results, status) {
+            geocoder.geocode( { 'address': address.val()}, function(results, status) {
                 if (status === google.maps.GeocoderStatus.OK) {
-                    jQuery('#dd_input_geolocate').val = '';
-                    jQuery('#locationLatLng').val = results[0].geometry.location.lat() + ',' + results[0].geometry.location.lng();
+                    jQuery('#dd_input_geolocate').val('');
+                    jQuery('#locationLatLng').val(
+                        results[0].geometry.location.lat() + ',' + results[0].geometry.location.lng()
+                    );
                 }
             });
         }
     }, 300);
 
     jQuery('#dd_searchfilter-ajaxloader').css({'display':'inline'});
-
     setTimeout(function(){
         jQuery('#dd_gmaps_locations_searchfilter_form ').submit();
     }, 800);
@@ -29,18 +32,19 @@
 
 var initAutoCompleteListener = function initAutoCompleteListener() {
     // Adds auto complete input and input to LatLng function after google suggest list selected
-    var input = jQuery('#dd_input_location_search');
+    var input = document.getElementById('dd_input_location_search');
     var options = {types:[]};
     var autocomplete = new google.maps.places.Autocomplete(input, options);
 
     google.maps.event.addListener(autocomplete, 'place_changed', function() {
         var pl = autocomplete.getPlace();
-        jQuery("#locationLatLng").val(pl.geometry.location.lat() + ',' + pl.geometry.location.lng()) ;
+        jQuery('#locationLatLng').val(
+            pl.geometry.location.lat() + ',' + pl.geometry.location.lng()
+        );
 
-        jQuery('#dd_input_geolocate').value = '';
+        jQuery('#dd_input_geolocate').val('');
 
-        jQuery('#dd_searchfilter-ajaxloader').css({"display":"inline"});
-
+        jQuery('#dd_searchfilter-ajaxloader').css({'display':'inline'});
         setTimeout(function(){
             jQuery('#dd_gmaps_locations_searchfilter_form ').submit();
         }, 400);
@@ -58,7 +62,9 @@ function clearDD_GMaps_Form() {
 }
 
 function showPosition(position) {
-    jQuery('#dd_input_geolocate').val = 'locate';
-    jQuery('#locationLatLng').val = position.coords.latitude + ',' + position.coords.longitude;
+    jQuery('#dd_input_geolocate').val('locate');
+    jQuery('#locationLatLng').val(
+        position.coords.latitude + ',' + position.coords.longitude
+    );
     jQuery('#dd_gmaps_locations_searchfilter_form').submit();
 }
